f9064ee8513c79dd5e0ad32451eb7474829b8a9f
Edrinem <edrinem0011@icloud.com>
2026-03-21 14:15:37 +0000

    journal file updates

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1= new Job();
+        job1._jobTitle ="Software Engineer";
+        job1._company = "Airtel";
+        job1._startYear = 2022;
+        job1._endYear =2026;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Supervisor";
+        job2._company = "MTN";
+        job2._startYear =2018;
+        job2._endYear =2026;
+
+        job1.Display();
+        job2.Display();
+
     }
 }
